2188709decba9a0cccb2ca41684e8cef5385ec29
qiuzhanghua <qiuzhanghua@icloud.com>
2021-07-27 16:29:37 +0800

    try DFS

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,78 @@
-use petgraph::graph::NodeIndex;
+use petgraph::algo::{dijkstra, min_spanning_tree};
+use petgraph::graph::{EdgeIndex, NodeIndex};
 use petgraph::visit::EdgeRef;
 use petgraph::{Graph, Undirected};
+use std::collections::{HashSet, VecDeque};
 
 fn main() {
-    let graph = init();
+    let mut graph = init3();
     println!("{:?}", graph);
     // let graph = init2();
     // println!("{:?}", graph);
 
-    let nbs = graph.neighbors(NodeIndex::new(5));
-    nbs.into_iter().for_each(|x| println!("{:?}", x));
-    let edges = graph.edges(NodeIndex::new(5));
-    edges.into_iter().for_each(|x| println!("{:?}", x.id()));
+    // let nbs = graph.neighbors(NodeIndex::new(5));
+    // nbs.into_iter().for_each(|x| println!("{:?}", x));
+    // let edges = graph.edges(NodeIndex::new(5));
+    // edges.into_iter().for_each(|x| println!("{:?}", x.id()));
+    // let node_map = dijkstra(&graph, NodeIndex::new(0), Some(NodeIndex::new(1)), |edge_ref| 1);
+    // println!("{:?}", node_map);
+    let longest = longest_path(&mut graph);
+    println!("{}", longest);
 }
 
-fn init() -> Graph<char, u32, Undirected, u32> {
-    let mut graph = Graph::<char, u32, Undirected>::new_undirected();
+fn next_node(graph: &Graph<char, i32, Undirected, u32>, edge_id: usize, curr: usize) -> usize {
+    match graph.edge_endpoints(EdgeIndex::new(edge_id)) {
+        None => usize::MAX,
+        Some((x1, x2)) => {
+            if x1.index() == curr {
+                x2.index()
+            } else {
+                x1.index()
+            }
+        }
+    }
+}
+
+fn longest_path(graph: &mut Graph<char, i32, Undirected, u32>) -> u32 {
+    if graph.node_count() <= 2 || graph.edge_count() < 1 {
+        return 0;
+    }
+    let start: usize = 0;
+
+    let mut start_edges = graph.edges(NodeIndex::new(start as usize));
+    for edge in start_edges {
+        println!("========");
+        let mut visited = Vec::<usize>::new();
+        let mut stack = VecDeque::<usize>::new();
+        let mut stack_of_node = VecDeque::<usize>::new();
+        stack_of_node.push_back(start);
+
+        stack_of_node.push_back(next_node(&graph, edge.id().index(), start));
+        stack.push_back(edge.id().index());
+        while !stack.is_empty() {
+            let e = stack.pop_back().unwrap();
+            let this_node = stack_of_node.pop_back().unwrap();
+            // let this_node = next_node(&graph, e, start);
+            if !visited.contains(&e) {
+                visited.push(e);
+                for x in graph.edges(NodeIndex::new(this_node)) {
+                    let id = x.id().index();
+                    stack.push_back(id);
+                    stack_of_node.push_back(next_node(&graph, id, this_node));
+                }
+            } else {
+                // println!("{:?}", &stack_of_node);
+            }
+            println!("visited: {:?}", &visited);
+            println!("stack: {:?}", &stack);
+            println!("node: {:?}", &stack_of_node);
+        }
+    }
+    0
+}
+
+fn init() -> Graph<char, i32, Undirected, u32> {
+    let mut graph = Graph::<char, i32, Undirected>::new_undirected();
     let vertx_a = graph.add_node('A');
     let vertx_b = graph.add_node('B');
     let vertx_c = graph.add_node('C');
@@ -43,8 +100,8 @@ fn init() -> Graph<char, u32, Undirected, u32> {
     graph
 }
 
-fn init2() -> Graph<char, u32, Undirected, u32> {
-    Graph::<char, u32, Undirected>::from_edges(&[
+fn init2() -> Graph<char, i32, Undirected, u32> {
+    Graph::<char, i32, Undirected>::from_edges(&[
         (0, 1, 5),
         (0, 3, 4),
         (0, 4, 3),
@@ -57,5 +114,15 @@ fn init2() -> Graph<char, u32, Undirected, u32> {
         (4, 7, 4),
         (5, 6, 4),
         (6, 7, 5),
+    ])
+}
+
+fn init3() -> Graph<char, i32, Undirected, u32> {
+    Graph::<char, i32, Undirected>::from_edges(&[
+        (0, 1, 1),
+        (0, 1, 2),
+        (0, 1, 4),
+        (1, 2, 8),
+        (1, 2, 16),
     ])
 }
